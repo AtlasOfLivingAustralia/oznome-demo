8b784e048b854050ee72ad8ceff6438dd9a87085
Mahmoud Sadeghi <sadeghim@gmail.com>
2015-06-04 15:25:49 +1000

    Visualisation Service - data format change

--- grails-app/controllers/au/org/ala/regions/VisualisationController.groovy
+++ grails-app/controllers/au/org/ala/regions/VisualisationController.groovy
@@ -8,7 +8,7 @@ class VisualisationController {
     SpeciesIpService speciesIpService
 
     def visualisation() {
-        def result = []
+        Map result =  new HashMap()
         def patentsList = []
         def species
         def patents
@@ -28,21 +28,21 @@ class VisualisationController {
         patentsList = patentsList.unique { patent -> patent.applicationNumber }
 
         //Patents by year
-        result += ["patentByYear", [["Period", "Number"]] + patentsList.countBy {
+        result.put("patentByYear", [["Period", "Number"]] + patentsList.countBy {
             it.filingDate[0..3].toInteger()
-        }.collect { k, v -> [k, v] }]
+        }.collect { k, v -> [k, v] })
 
         //Patents by status
-        result += ["patentsByStatus", [["Status", "Number"]] + patentsList.countBy {
+        result.put("patentsByStatus", [["Status", "Number"]] + patentsList.countBy {
             it.filingStatus
-        }.collect { k, v -> [k, v] }]
+        }.collect { k, v -> [k, v] })
 
         // Top 20 Applicants
         def applicants = patentsList.countBy { it.applicants }.sort { -it.value }
         def i = 0
-        result += ["top20Applicants", [["Entity", "Number"]] + applicants.findAll {
+        result.put("top20Applicants", [["Entity", "Number"]] + applicants.findAll {
             i++ < 20
-        }.collect { k, v -> [k, v] }]
+        }.collect { k, v -> [k, v] })
 
         render result as JSON
     }
